978f9b822541b4a2f7ca306333ac783467c98b00
George Sharpe <89303209+sharpegeorge@users.noreply.github.com>
2023-07-02 10:16:58 +0100

    Gives rref with known errors

--- solver.py
+++ solver.py
@@ -1,29 +1,12 @@
 import numpy as np
+import sympy
+import sys
 
-# columns of matrix - first row of board is index 0-8, second row 9-17, etc
-matrixWithoutGroups = np.array([
-    # columns of board
-    np.concatenate((np.repeat(1, 9), np.repeat(0, 81 - 9), [45])),
-    np.concatenate((np.repeat(0, 9), np.repeat(1, 9), np.repeat(0, 81 - 18), [45])),
-    np.concatenate((np.repeat(0, 18), np.repeat(1, 9), np.repeat(0, 81 - 27), [45])),
-    np.concatenate((np.repeat(0, 27), np.repeat(1, 9), np.repeat(0, 81 - 36), [45])),
-    np.concatenate((np.repeat(0, 36), np.repeat(1, 9), np.repeat(0, 81 - 45), [45])),
-    np.concatenate((np.repeat(0, 45), np.repeat(1, 9), np.repeat(0, 81 - 54), [45])),
-    np.concatenate((np.repeat(0, 54), np.repeat(1, 9), np.repeat(0, 81 - 63), [45])),
-    np.concatenate((np.repeat(0, 63), np.repeat(1, 9), np.repeat(0, 81 - 72), [45])),
-    np.concatenate((np.repeat(0, 72), np.repeat(1, 9), [45])),
 
-    # rows of board
-
-    # boxes of board
-])
-
+np.set_printoptions(threshold=sys.maxsize)
 
 def solve(groups, sumPerGroup):
-    groups = np.array([[0, 1, 79], [79, 7, 80]])
-    sumPerGroup = np.array([19, 21])
-
-    numGroups = groups.shape[0]
+    numGroups = len(groups)
     matrix = np.zeros(shape=(27 + numGroups, 82))
 
     # equations for each board column
@@ -77,7 +60,15 @@ def solve(groups, sumPerGroup):
         matrixRow = groupNum + 27
         matrix[matrixRow] = newRow
 
-    print(matrix)
 
 
-solve(5, 5)
+    print(sympy.Matrix(matrix).rref())
+
+
+groups = [[0, 9], [1, 2], [3, 4, 12, 13], [5, 6, 14, 15, 24], [7, 8], [10, 11, 18, 19],
+          [20, 21], [22, 23, 32, 33], [16, 17, 26], [25, 34], [27, 36], [28, 37], [29, 30, 31], [35, 43, 44, 52, 53],
+        [38, 39], [40, 41], [42, 51, 60, 59], [45, 46], [47, 56], [48, 49, 50], [54, 63, 72] ,[55, 64, 65], [57,58],
+          [61, 62], [66, 67, 75, 76], [68, 77], [69,70], [71, 80], [73,74], [78, 79]]
+
+sumPerGroup = np.array([8, 15, 20, 21, 10, 16, 13, 22, 20, 7, 16, 7, 6, 27, 9, 14, 18, 14, 8, 14, 11, 19, 9, 11, 20, 14, 9, 10, 10, 7])
+solve(groups, sumPerGroup)
