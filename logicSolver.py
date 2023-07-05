0ad4624cdb75b3d918e1e6d9c910fab388b84d17
George Sharpe <89303209+sharpegeorge@users.noreply.github.com>
2023-07-05 11:26:05 +0100

    Started update cycle of logicSolver
    Need to add further checks and recursive calls

--- logicSolver.py
+++ logicSolver.py
@@ -39,7 +39,25 @@ def setPossibleCellValues(values, cellIndex=None, cellRow=None, cellCol=None):
         indexCorrected = int(value - 1)
         possibleBoardValues[cellRow, cellCol, indexCorrected] = True
 
+    updatePossibleValues(values, cellRow, cellCol)
     # triggers a check to see if it affects other possible cells
+
+
+# NEED TO MAKE FUNCTION TO SET INDIVIDUAL POSSIBLE VALUE OF A CELL
+
+
+def updatePossibleValues(values, cellRow, cellCol):
+    if len(values) == 1:
+        print("placeholder updatePossibleValues ln50")
+        # remove value from column, row, box and group (will need to take cages/pairings into account)
+
+    # in column, row, box or group (individually tho):
+    # check for v (num values) cells with identical values
+        # if yes then remove values from all other set in the given column, row, box or group
+
+    # something else i forgor
+
+
 
 
 def logicSolve(board, groups, sumPerGroup):
